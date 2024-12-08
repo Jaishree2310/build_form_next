59c6796e81eed4b477f627d022f8475c59f89134
Jaishree2310 <jaishrees23102001@gmail.com>
2024-12-08 16:55:26 +0530

    feat: form dnd added

---
+++ src/utils/dnd-utils.ts
@@ -0,0 +1,12 @@
+import { useSensors, useSensor, PointerSensor, KeyboardSensor } from '@dnd-kit/core';
+import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
+
+export const useDragSensors = () => {
+  return useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+};
+
